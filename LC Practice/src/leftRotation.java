00d5b3f4f7b97960314d4932e078445bed031ccb
Eric Wang <58147496+ericywang@users.noreply.github.com>
2020-09-22 21:35:14 -0400

    Update leftRotation.java

--- LC Practice/src/leftRotation.java
+++ LC Practice/src/leftRotation.java
@@ -1,28 +1,48 @@
 public class Main
 {
-	public static void main(int d, String[] args) {
+	public static void main(String[] args) {
 	    
-	    leftRotation(args, d);
+	    System.out.println(leftRotation(10, "beabeefeab"));
+	    System.out.println(leftRotation(6, "aaaaaa"));
 	    
     }
     
-    public static void leftRotation(int [] n, int d) {
-        for(int x = 0; x < d/2; x++){
-            int temp = n[x];
-            n[x] = n[d- 1 -x];
-            n[d- 1 -x] = temp;
+    public static int leftRotation(int length, String s) {
+        int[][] numMatrix = new int[26][26];
+        char[][] charMatrix = new char[26][26];
+        int max = 0;
+
+        for(int i = 0; i < length; i++){
+            for(int j = 0; j < 26; j++){
+                if(charMatrix[s.charAt(i) - 'a'][j] == s.charAt(i)){
+                    numMatrix[s.charAt(i) - 'a'][j] = -1;
+                }
+                else{
+                    charMatrix[s.charAt(i) - 'a'][j] = s.charAt(i);
+                    if(numMatrix[s.charAt(i) - 'a'][j] == -1) continue;
+                    else{
+                        numMatrix[s.charAt(i) - 'a'][j]++;
+                    }
+                }
+
+                if(charMatrix[j][s.charAt(i) - 'a'] == s.charAt(i)){
+                    numMatrix[j][s.charAt(i) - 'a'] = -1;
+                }
+                else{
+                    charMatrix[j][s.charAt(i) - 'a'] = s.charAt(i);
+                    if(numMatrix[j][s.charAt(i) - 'a'] == -1) continue;
+                    else{
+                        numMatrix[j][s.charAt(i) - 'a']++;
+                    }
+                }
+            }
         }
-        for(int x = d; x < n.length /2; x++){
-            int temp = n[x];
-            n[x] = n[n.length - 1 -x];
-            n[n.length - 1 -x] = temp;
+
+        for(int x = 0; x < 26; x++){
+            for(int y = 0; y < 26; y++){
+                max = Math.max(numMatrix[x][y], max);
+            }
         }
-        for(int x = 0; x < n.length /2; x++){
-            int temp = n[x];
-            n[x] = n[n.length - 1 -x];
-            n[n.length - 1 -x] = temp;
-        }
-        for(int x: n)
-            System.out.println(x);
+        return max;  
 	}
 }
