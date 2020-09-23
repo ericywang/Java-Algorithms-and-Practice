65286eff6fa2b48ba39e229c76952f1bc15b75f5
Eric Wang <58147496+ericywang@users.noreply.github.com>
2020-09-22 21:36:53 -0400

    Update leftRotation.java

--- LC Practice/src/leftRotation.java
+++ LC Practice/src/leftRotation.java
@@ -2,47 +2,33 @@ public class Main
 {
 	public static void main(String[] args) {
 	    
-	    System.out.println(leftRotation(10, "beabeefeab"));
-	    System.out.println(leftRotation(6, "aaaaaa"));
-	    
+	    leftRotation(5, 4);
+	    leftRotation(10, 1);
+	    leftRotation(12, 0);
     }
     
-    public static int leftRotation(int length, String s) {
-        int[][] numMatrix = new int[26][26];
-        char[][] charMatrix = new char[26][26];
-        int max = 0;
-
-        for(int i = 0; i < length; i++){
-            for(int j = 0; j < 26; j++){
-                if(charMatrix[s.charAt(i) - 'a'][j] == s.charAt(i)){
-                    numMatrix[s.charAt(i) - 'a'][j] = -1;
-                }
-                else{
-                    charMatrix[s.charAt(i) - 'a'][j] = s.charAt(i);
-                    if(numMatrix[s.charAt(i) - 'a'][j] == -1) continue;
-                    else{
-                        numMatrix[s.charAt(i) - 'a'][j]++;
-                    }
-                }
-
-                if(charMatrix[j][s.charAt(i) - 'a'] == s.charAt(i)){
-                    numMatrix[j][s.charAt(i) - 'a'] = -1;
-                }
-                else{
-                    charMatrix[j][s.charAt(i) - 'a'] = s.charAt(i);
-                    if(numMatrix[j][s.charAt(i) - 'a'] == -1) continue;
-                    else{
-                        numMatrix[j][s.charAt(i) - 'a']++;
-                    }
-                }
-            }
+    public static void leftRotation(int arraySize, int d) {
+        int[] n = new int[arraySize];
+        for(int num = 0; num < arraySize; num++){
+            n[num] = num+1;
         }
-
-        for(int x = 0; x < 26; x++){
-            for(int y = 0; y < 26; y++){
-                max = Math.max(numMatrix[x][y], max);
-            }
+        for(int x = 0; x < d/2; x++){
+            int temp = n[x];
+            n[x] = n[d- 1 -x];
+            n[d- 1 -x] = temp;
         }
-        return max;  
+        for(int x = d; x < n.length /2; x++){
+            int temp = n[x];
+            n[x] = n[n.length - 1 -x];
+            n[n.length - 1 -x] = temp;
+        }
+        for(int x = 0; x < n.length /2; x++){
+            int temp = n[x];
+            n[x] = n[n.length - 1 -x];
+            n[n.length - 1 -x] = temp;
+        }
+        for(int x: n)
+            System.out.print(x+ " ");
+        System.out.println();
 	}
 }
